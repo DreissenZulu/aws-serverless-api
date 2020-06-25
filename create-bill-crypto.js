fe3d09baa2908e375412ac0da951f270df38baf4
Alex Khoa Le <leduykhoa@hotmail.ca>
2020-06-25 10:50:27 -0700

    Initialized bill function for Coinpayments API

---
+++ create-bill-crypto.js
@@ -0,0 +1,20 @@
+import handler from "./libs/handler-lib";
+import { calculateCost } from "./libs/create-bill-lib";
+
+export const main = handler(async (event) => {
+    const billingOptions = JSON.parse(event.body);
+    const amount = calculateCost(billingOptions.options);
+
+    const stripe = stripePackage(process.env.stripeSecretKey);
+
+    const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card'],
+        setup_future_usage: 'on_session',
+        // Verify your integration in this guide by including this parameter
+        metadata: {integration_check: 'accept_a_payment'},
+  });
+
+  return paymentIntent;
+});
