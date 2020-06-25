bf1a57d19d4c5bae121d2a73d097d11a016137a3
Alex Khoa Le <leduykhoa@hotmail.ca>
2020-06-25 10:52:18 -0700

    Proper credit where credit is due

--- encrypt-key.js
+++ encrypt-key.js
@@ -1,7 +1,7 @@
 import handler from "./libs/handler-lib";
 
 export const main = handler(async (event) => {
-    // Function created by Filip Dimitrovski
+    // Function based on encryption function by Filip Dimitrovski
     const request = event.body + `&key=${process.env.COIN_PAYMENTS_KEY}`;
     const encryptedKey = require('crypto')
         .createHmac('sha512', process.env.COIN_PAYMENTS_SECRET_KEY)
