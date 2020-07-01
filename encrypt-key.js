01d4a16f84c28404a64b6898b87f68d018a275ac
Alex Khoa Le <leduykhoa@hotmail.ca>
2020-07-01 13:53:12 -0700

    Reverted request constant to expect the coinpayments public key from the request body

--- encrypt-key.js
+++ encrypt-key.js
@@ -2,7 +2,7 @@ import handler from "./libs/handler-lib";
 
 export const main = handler(async (event) => {
     // Function based on encryption function by Filip Dimitrovski
-    const request = event.body + `&key=${process.env.COIN_PAYMENTS_KEY}`;
+    const request = event.body;
     const encryptedKey = require('crypto')
         .createHmac('sha512', process.env.COIN_PAYMENTS_SECRET_KEY)
         .update(request)
